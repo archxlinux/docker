0b896b12e1a173ff4b58927a6aa17900ceb1561d
Alessio <alessio@archxlinux.com>
2023-08-06 09:21:49 +0200

    ci fixes

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ set -e
 setup_root() {
     # Set up the new root directory
     newroot=./root
-    mkdir -p $newroot
-
+    mkdir $newroot
     pkgs='archxlinux-keyring bash coreutils pacman'
     ignore_pkgs='cryptsetup,jfsutils,lvm2,mdadm,nano,netctl,reiserfsprogs,s-nail,vi,xfsprogs,man-pages,systemd,base-devel'
     yes | pacman -Sy base-devel arch-install-scripts --noconfirm
     yes | pacstrap -i $newroot $pkgs --noconfirm --ignore $ignore_pkgs
+    cp -r /etc/pacman* ./root/etc/
 
 }
 
